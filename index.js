b41b2854388ca2de5178fb15991c70cf2d1168c5
OmklthomAmara <amaraomklthom@gmail.com>
2020-10-26 23:13:58 +0200

    add button updating the dom

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function makeNote(message) {
     date: new Date(),
     id: counter++,
     checked: false,
+    trash:false  
   };
 }
 
@@ -15,7 +16,14 @@ function addNote(note) {
 }
 
 function deleteNote(id) {
-  notes = notes.filter((note) => note.id !== id);
+  //notes = notes.filter((note) => note.id !== id);
+  notes.some((note) => {
+    if (note.id === id) {
+      note.delete = true;
+      return true;
+    }
+    return false;
+  });
 }
 
 function checkNote(id) {
@@ -31,9 +39,33 @@ function checkNote(id) {
 // Update DOM
 function updateNotes() {}
 
+
+
 document.querySelector('.addButton').addEventListener('click', (e) => {
   const input = document.querySelector('.input').value;
+  const list = document.querySelector('.list');
   const note = makeNote(input);
   addNote(note);
-  updateNotes();
+  
+  const listElement = document.createElement("li");
+  const label = document.createElement('label');
+  const checkBtn = document.createElement('input');
+  checkBtn.setAttribute("type", "checkbox");
+  const text = document.createTextNode(input);
+  const span = document.createElement("span");
+  const spanTxt = document.createTextNode("delete");
+  listElement.appendChild(label);
+  label.appendChild(checkBtn);
+  label.appendChild(text);
+  span.appendChild(spanTxt);
+  label.appendChild(span);
+  list.appendChild(listElement);
+  
+  document.querySelector('.input').value = "";
+
+   
 });
+
+
+
+
