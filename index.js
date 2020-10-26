01d5481adadc58206d7bdf53d5301009dbda03f6
nizarhalloun <nshalloun@hotmail.com>
2020-10-26 15:56:34 +0200

    simple add

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const notes = [];
+let counter = 0;
 
 function makeNote(message) {
   return {
     message,
     date: new Date(),
-    id: 3,
+    id: counter++,
+    checked: false,
   };
 }
 
-function addNote(note) {}
+function addNote(note) {
+  notes.push(note);
+}
 
-function deleteNote(id) {}
+function deleteNote(id) {
+  notes = notes.filter((note) => note.id !== id);
+}
 
-function checkNote(id) {}
+function checkNote(id) {
+  notes.some((note) => {
+    if (note.id === id) {
+      note.checked = true;
+      return true;
+    }
+    return false;
+  });
+}
 
-function updateNotes(params) {}
+// Update DOM
+function updateNotes() {}
 
 document.querySelector('.addButton').addEventListener('click', (e) => {
   const input = document.querySelector('.input').value;
   const note = makeNote(input);
   addNote(note);
+  updateNotes();
 });
